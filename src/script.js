d94bf0d6af858c8bffbc84721c4908daa89bdaee
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-12 19:34:16 -0300

    Salvamento do nome em local storage

--- src/script.js
+++ src/script.js
@@ -2,5 +2,8 @@
 const botao = document.getElementById('botao');
 botao.addEventListener('click', () => {
     const nome = document.getElementById('nome').value;
+    localStorage.setItem('nome', nome);
+    // localStorage.getItem('nome'); para pegar o nome novamente ou
+    // apenas 'nome' por conta da variável criada
     alert(`Seja bem vindo ${nome} !`);
 })
